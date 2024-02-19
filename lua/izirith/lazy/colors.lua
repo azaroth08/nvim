b254826efaacdda29264589ddd14d0e51388b1e4
izirith <jay.jay0088@gmail.com>
2024-02-19 02:07:21 -0500

    a few more refinements

--- lua/izirith/lazy/colors.lua
+++ lua/izirith/lazy/colors.lua
@@ -1,48 +1,68 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine"
+	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 
 end
 
 return {
     {
         "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+        opts = {
+            dim_inactive = false,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+            on_highlights = function(hl, c)
+                hl.LspDiagnosticsDefaultHint = {fg = c.blue }
+                hl.LspDiagnosticsDefaultInformation = {fg = c.blue}
+                hl.LspDiagnosticsDefaultWarning = { fg = c.yellow}
+                hl.LspDiagnosticsDefaultError = {fg = c.red}
+                --remove telescope background
+                hl.TelescopeNormal = {bg =  "NONE"}
+                hl.TelescopeBorder = {bg  = "NONE"}
+                hl.TelescopePromptBorder = { bg = "NONE" }
+                hl.TelescopePreviewBorder = { bg = "NONE" }
+            end,
+        },
     },
-
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-            })
-
-            vim.cmd("colorscheme rose-pine")
-
-            ColorMyPencils()
-        end
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {
+            options = {
+                theme = "tokyonight",
+            }
+        }
     },
-
-
+    {
+        "kdheepak/tabline.nvim",
+        opts = {
+            -- Defaults configuration options
+            enable = true,
+            options = {
+                -- If lualine is installed tabline will use separators configured in lualine by default.
+                -- These options can be used to override those settings.
+                section_separators = { "", "" },
+                component_separators = { "", "" },
+                max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
+                show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+                show_devicons = true, -- this shows devicons in buffer section
+                show_bufnr = false, -- this appends [bufnr] to buffer section,
+                show_filename_only = false, -- shows base filename only instead of relative path in filename
+                modified_icon = "+ ", -- change the default modified icon
+                modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+                show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+            },
+        },
+    },
 }
