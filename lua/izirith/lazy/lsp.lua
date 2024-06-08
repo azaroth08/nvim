459232907ff298de33bbe9af7ab802f5b2b1c06f
izirith <jay.jay0088@gmail.com>
2024-06-08 18:14:15 -0400

    slight changes to lsp

--- lua/izirith/lazy/lsp.lua
+++ lua/izirith/lazy/lsp.lua
@@ -1,8 +1,8 @@
 return {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason-lspconfig.nvim",
     dependencies = {
         "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
@@ -10,42 +10,30 @@ return {
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+        "j-hui/fidget.nvim"
     },
-
     config = function()
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
-                "tsserver",
+                "clangd",
+                "tsserver"
             },
             handlers = {
-                function(server_name) -- default handler (optional)
-
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
+                function (server_name)
+                    require("lspconfig")[server_name].setup {}
                 end,
 
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
+                    lspconfig.lua_ls.setup{
                         settings = {
                             Lua = {
                                 diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                    globals = { "vim" }
                                 }
                             }
                         }
@@ -54,12 +42,13 @@ return {
             }
         })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp = require("cmp")
+        local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
@@ -75,17 +64,8 @@ return {
                 { name = 'buffer' },
             })
         })
-
         vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
+            virtual_text=true
         })
-    end
+   end
 }
